676b5c8603f925c9bb921f4d19cd01724d4e8641
Pratham04-CE <241263107033setice@gmail.com>
2026-07-19 23:55:06 +0530

    database error find

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,13 +1,11 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-echo "Script started...";
+//echo "Script started...";
 
 $conn = mysqli_connect("bexfkwadpmmxuduevla3-mysql.services.clever-cloud.com", "u4t59ngvwo6ye93r", "Im9MZghH3EFepvQtVW8h", "bexfkwadpmmxuduevla3", 3306);
 
 if (!$conn) {
     echo "<br>Connection failed: " . mysqli_connect_error();
-} else {
-    echo "<br>Connected successfully!";
 }
 ?>
