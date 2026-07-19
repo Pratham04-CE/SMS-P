f24454d857971447e47db93a68aaefa7f5bd65db
Pratham04-CE <241263107033setice@gmail.com>
2026-07-19 22:16:53 +0530

    database checkup

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,21 +1,19 @@
 <?php
-// Database configuration
+// Debugging ON
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = "bexfkwadpmmxuduevla3-mysql.services.clever-cloud.com";
 $user = "u4t59ngvwo6ye93r";
 $pass = "Im9MZghH3EFepvQtVW8h";
 $dbname = "bexfkwadpmmxuduevla3";
 $port = 3306;
 
-// Connection error report karne ke liye
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
-try {
-    $conn = mysqli_connect($host, $user, $pass, $dbname, $port);
-} catch (mysqli_sql_exception $e) {
-    die("Database Connection Error: " . $e->getMessage());
-}
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database Connected Successfully!";
 }
 ?>
